fce875f7af1ac579e42b8e211b2f294517701b9c
ricardo Oliveira <81876646+RICARDO-TVT@users.noreply.github.com>
2021-10-04 19:56:47 -0300

    Add files via upload

--- BuildNumbersParse_git.ps1
+++ BuildNumbersParse_git.ps1
@@ -7,7 +7,7 @@
 $ErrorActionPreference = 'Stop'
 
  # Create a PSCredential Object using the "User" and "Password" parameters that you passed to the job
-$SecurePassword = $env:pass | ConvertTo-SecureString -AsPlainText -Force
+$SecurePassword = $env:password | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:user, $SecurePassword
   
 $sqlVersion = "ALL"
