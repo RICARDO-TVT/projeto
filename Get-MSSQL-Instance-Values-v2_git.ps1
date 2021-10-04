dd63a43b1b032f96c1cc8ee7fbf738f80a835b96
ricardo Oliveira <81876646+RICARDO-TVT@users.noreply.github.com>
2021-10-04 19:46:45 -0300

    Add files via upload

--- Get-MSSQL-Instance-Values-v2_git.ps1
+++ Get-MSSQL-Instance-Values-v2_git.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Stop'
 
  # Create a PSCredential Object using the "User" and "Password" parameters that you passed to the job
-$SecurePassword = $env:pass | ConvertTo-SecureString -AsPlainText -Force
+$SecurePassword = $env:Password | ConvertTo-SecureString -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:user, $SecurePassword
 
 $webRequest = Invoke-WebRequest https://raw.githubusercontent.com/RICARDO-TVT/projeto/main/Settingsteste.txt -UseBasicParsing
