53f46286434059034974f4fc6a4b514f0ab21eec
ricardo Oliveira <81876646+RICARDO-TVT@users.noreply.github.com>
2021-10-19 21:59:45 -0300

    Delete Download.ps1

--- Download.ps1
+++
@@ -1,9 +0,0 @@
-﻿Add-Type -Assembly System.Web # [System.Web.HttpUtility]::UrlEncode() needs this
-
-$SqlVersion = "2017"
-$Query = "select * where A='" + $SqlVersion + "'"
-$URL   = "https://docs.google.com/spreadsheets/d/16Ymdz80xlCzb6CwRFVokwo0onkofVYFoSkc7mYe6pgw/gviz/tq?tq=" `
-       + [System.Web.HttpUtility]::UrlEncode($Query) `
-       + "&tqx=out:csv"
-
-Invoke-WebRequest $URL -OutFile "C:\Temp\SqlServerBuilds.csv"
